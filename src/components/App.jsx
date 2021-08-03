c85fc3ed298b34d2e052feff726b201f3af1a1d1
Garv nanwani <garvwashere@gmail.com>
2021-08-04 03:23:32 +0530

    completed auth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Home from '../pages/home'
+import Login from '../pages/login'
+import Signup from '../pages/signup'
+import NotFound from '../pages/404'
+import Loading from './Loading'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
-  return (
-    <div>
-    </div>
-  )
+    return (
+        <div>
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/signup" component={Signup} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Router>
+        </div>
+    )
 }
 
 export default App
