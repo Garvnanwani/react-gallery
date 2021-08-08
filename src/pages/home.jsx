7f1e3fcc6f1c0eedaec39f3a36b6a17729fb7cbc
Garv nanwani <garvwashere@gmail.com>
2021-08-09 02:29:49 +0530

    fix: app name

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <>
             <h1 className="m-auto mb-3 text-3xl text-center mt-7">
-                Welcome To NG Gallery
+                Welcome To React Gallery
             </h1>
             <section className="flex justify-center mx-auto text-center">
                 <div className="w-10/12">
